a803939f520094d9630ad08b87a18393d6de52ca
PomanoB <romanov4400@gmail.com>
2012-09-16 22:33:40 +0400

    Bad models deleted, new models added. close #2

--- data_models.js
+++ data_models.js
@@ -10,7 +10,6 @@ exports.models = [
 		alias: 'norm60-corpus-all',
 		fileName: 'data/norm60-corpus-all.csv'
 	},
-	
 	{
 		name: "PatternSim: Efreq",
 		alias: 'pairs-all-raw-sort',
@@ -41,30 +40,38 @@ exports.models = [
 		alias: 'raw-wacky',
 		fileName: 'data/raw-wacky.csv'
 	},
-		{
+
+
+	{
 		name: "Cos-MKNN-5",
-		alias: 'cos-mknn-5-327167',
-		fileName: 'data/cos-mknn-5-327167.csv'
+		alias: 'cos-mknn-5-12130',
+		fileName: 'data/cos-mknn-5-12130.csv'
 	},
 	{
+		name: "Cos-MKNN-10",
+		alias: 'cos-mknn-10-12130',
+		fileName: 'data/cos-mknn-10-12130.csv'
+	},
+	{
+		name: "Cos-MKNN-20",
+		alias: 'cos-mknn-20-12130',
+		fileName: 'data/cos-mknn-20-12130.csv'
+	},
+
+	{
 		name: "Overlap-MKNN-5",
-		alias: 'overlap-mknn-5-32716',
-		fileName: 'data/overlap-mknn-5-327167.csv'
+		alias: 'overlap-mknn-5-12130',
+		fileName: 'data/overlap-mknn-5-12130.csv'
 	},
 	{
 		name: "Overlap-MKNN-10",
-		alias: 'overlap-mknn-10-327167',
-		fileName: 'data/overlap-mknn-10-327167.csv'
+		alias: 'overlap-mknn-10-12130',
+		fileName: 'data/overlap-mknn-10-12130.csv'
 	},
 	{
 		name: "Overlap-MKNN-20",
-		alias: 'overlap-mknn-20-327167',
-		fileName: 'data/overlap-mknn-20-327167.csv'
-	},
-	{
-		name: "Overlap-MKNN-50",
-		alias: 'overlap-mknn-50-327167',
-		fileName: 'data/overlap-mknn-50-327167.csv'
+		alias: 'overlap-mknn-20-12130',
+		fileName: 'data/overlap-mknn-20-12130.csv'
 	},
 
 ];
