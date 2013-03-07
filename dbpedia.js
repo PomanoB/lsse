38e8e7722a1b1ffdb8d1d9991a7070948b4cbb26
PomanoB <romanov4400@gmail.com>
2013-03-07 22:02:11 +0400

    Issue #27 - WIP

--- dbpedia.js
+++ dbpedia.js
@@ -2,34 +2,42 @@ var request = require('request'), fs = require('fs');
 
 var dbPedia = {
 
-	getDefinition: function(word, callback) {
+	getDefinition: function(word, callback, disambiguates) {
 
 		word = word.charAt(0).toUpperCase() + word.slice(1);
 		word = word.replace(/ /g, '_');
 		request('http://dbpedia.org/data/' + encodeURIComponent(word) + '.json', {
 			headers: {'User-Agent': 'LSSE'}
 		}, function (error, response, body) {
+			// console.log(response.statusCode, body);
 			if (response.statusCode == 200)
 			{
 				var data = JSON.parse(body);
 				var name = 'http://dbpedia.org/resource/' + word;
 				if (typeof data[name] == "undefined")
 				{
-					callback('Not found!', null);
+					callback('Not found1!', null);
 					return;
 				}
 				if (typeof data[name]['http://dbpedia.org/ontology/wikiPageDisambiguates'] != "undefined")
 				{
-					var newWord = data[name]['http://dbpedia.org/ontology/wikiPageDisambiguates'][0]['value'].substring(28);
-					dbPedia.getDefinition(newWord, callback);
+					var dis = [], i, wikiPageDisambiguates = data[name]['http://dbpedia.org/ontology/wikiPageDisambiguates'];
+					for(i = 0; i < wikiPageDisambiguates.length; i++)
+						dis.push(wikiPageDisambiguates[i].value.substring(28));
+
+					var newWord = dis[0];
+					dbPedia.getDefinition(newWord, callback, dis);
 					return;
 				}
 				if (typeof data[name]['http://dbpedia.org/ontology/abstract'] == "undefined")
 				{
-					callback('Not found!', null);
+					callback('Not found2!', null);
 					return;
 				}
-				var result = {word: word, en: null, ru: null, image: null}, i, abstract = data[name]['http://dbpedia.org/ontology/abstract'];
+				var result = {word: word, en: null, ru: null, image: null, disambiguates: []}, i, abstract = data[name]['http://dbpedia.org/ontology/abstract'];
+				if (typeof disambiguates != "undefined")
+					result.disambiguates = disambiguates;
+
 				for(i = 0; i < abstract.length; i++)
 				{
 					if (abstract[i].lang == "en" || abstract[i].lang == "ru")
@@ -44,14 +52,14 @@ var dbPedia = {
 				// fs.writeFileSync('3.js', body);
 			}
 			else
-				callback('Not found!', null);
+				callback('Not found3!', null);
 		});				
 	}
 }
 module.exports = dbPedia;
 
 // dbPedia.getDefinition('Linux', function(error, result){
-// // dbPedia.getDefinition('Morphology_(biology)', function(){
-// // dbPedia.getDefinition('Morphology', function(){
-// 	console.log(arguments);
+// dbPedia.getDefinition('Morphology_(biology)', function(error, result){
+// dbPedia.getDefinition('Morphology', function(error, result){
+	// console.log(error, result);
 // })
